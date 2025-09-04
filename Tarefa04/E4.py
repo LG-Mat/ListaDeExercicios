1f7a4b2580fff66ad1f3ac3cb1c00e9892c7251e
Luís Gustavo M. Odriozola <lgmatiotti@gmail.com>
2025-09-04 14:16:57 -0300

    Update E4.py

--- Tarefa04/E4.py
+++ Tarefa04/E4.py
@@ -3,12 +3,15 @@ import requests
 
 CSV_URL = "https://raw.githubusercontent.com/emmendorfer/idwr/main/demo/datasets/amazon.csv"
 
-with requests.Session() as s:
-    download = s.get(CSV_URL)
+def ler_url(CSV_URL):
+    with requests.Session() as s:
+        download = s.get(CSV_URL)
 
-    decoded_content = download.content.decode('utf-8')
+        decoded_content = download.content.decode('utf-8')
 
-    reader = csv.reader(decoded_content.splitlines(), delimiter=',')
-    mylist = list(reader)
-    for row in mylist:
-        print(row)
+        reader = csv.reader(decoded_content.splitlines(), delimiter=',')
+        mylist = list(reader)
+        for row in mylist:
+            print(row)
+
+ler_url(CSV_URL)
